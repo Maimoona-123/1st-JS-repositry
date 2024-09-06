b2f6e96361a6bf03fa6b2e1eaab23bc7d35411b0
maimoona <maimoonaaltaf7@gmail.com>
2024-09-06 05:11:29 -0700

    Add files

---
+++ app.js
@@ -0,0 +1,96 @@
+// Chap-1   ( ALERTS )
+
+// Q-1
+// alert("Welcome our website!");
+
+// Q-2
+// alert("Error! please enter a valid password");
+
+// Q-3
+// alert("Welcome to JS land..... \n Happy coding");
+
+// Q-4
+// alert("Happy coding! \n Prevent this page from creating dialogs");
+
+// Q-5
+// var msg ="Hello... I can run JS through my web browser's console";
+// console.log (msg);
+
+// Q-6 & 7
+// Theory 
+
+
+
+// Chap-2    ( VARIABLES FOR STRING)
+
+
+// Q-1
+// var username = "Johne Doe";
+// alert (username);
+
+
+// Q-3
+// var message = "Hello World..";
+// alert (message);
+
+
+// // Q-4
+// var name ="Maimoona" ;
+// alert (name);
+
+// var age ="15 years old" ;
+// alert (age) ;
+
+// var work ="Certified Web Development..." ;
+// alert (work) ;
+
+
+// Q-5
+// var pizza = "pizza \n pizz \n piz \n pi \n p" ;
+// alert (pizza) ;
+
+
+// Q-6
+// var email = "maimonaaltaf7@gmail.com" ;
+// alert (email) ;
+
+
+// Q-7
+// var JS = "A smarter way to learn JavaScript”." ;
+// alert (JS) ;
+
+
+// Q-8
+// document.write ( "Yah! i can write HTML content through JavaScript" )
+
+// Q-9
+var diagram = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert ( diagram )
+
+
+
+// Chap-3   ( VARIABLES FOR NUMBERS )
+
+
+// Q-1
+// var age = "I am 15 years old"
+// alert (age)
+
+// Q-2
+var visited = "you have visited this site 10 times" ;
+alert ( visited ) ;
+
+
+// Q-3
+// document.write ( "My birth year is 2009 " )
+
+
+// Q-4
+// document.write ( "Muhammad Ahmed orderd 8 T-shirt(s) on daraz cloth store " )
+
+
+
+// chap-4    ( VARIABLES NAME: LEGAL & ILLEGAL)
+
+document.write ( "Rules for naming JS variables." )
+document.write ( " Variable names can only contain Numbers, $ ,under-score and letter" )
